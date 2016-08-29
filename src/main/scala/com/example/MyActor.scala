33ad8b0457523ed1ba1a4657033d0ca76471ab54
shigemk2 <i.am.shige@gmail.com>
2016-08-30 03:53:15 +0900

    Add MyActor

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -1,5 +1,32 @@
 package com.example
 
-class MyActor {
+import akka.actor.{Actor, ActorIdentity, Identify, Props}
+import akka.event.Logging
 
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  val identifyId = 1
+  val child = context.actorOf(Props[MyActor2], name = "myChild")
+
+  var lastSender = context.system.deadLetters
+
+  def receive = {
+    case "search" => {
+      context.actorSelection("/user/myActor/myChild") ! Identify(identifyId)
+
+      lastSender = sender
+    }
+
+    case ActorIdentity(`identifyId`, Some(ref)) => {
+      log.info("found")
+      lastSender ! ref
+    }
+    case s: String => {
+      log.info(s)
+      child ! s
+    }
+    case _ => {
+    }
+  }
 }
