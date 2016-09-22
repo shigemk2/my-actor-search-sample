09a88a8f2843d7c2f1f3bd37b66e361e749a4524
shigemk2 <i.am.shige@gmail.com>
2016-09-22 19:25:50 +0900

    Add comments

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -6,21 +6,26 @@ import akka.event.Logging
 class MyActor extends Actor {
   val log = Logging(context.system, this)
 
-  val identifyId = 1
+  val identifyId = 1 // 問い合わせ番号
   val child = context.actorOf(Props[MyActor2], name = "myChild")
 
   var lastSender = context.system.deadLetters
 
   def receive = {
     case "search" => {
-      context.actorSelection("/user/myActor/myChild") ! Identify(identifyId)
+      context.actorSelection("/user/myActor/myChild") ! Identify(identifyId) // 絶対パス
+
+      // その他の指定方法
+      // context.actorSelection("../myActor/myChild") ! Identify(identifyId) // 相対パス
+      // context.actorSelection("myChild") ! Identify(identifyId) // 相対パス (`./myChild` の意味)
+      // context.actorSelection("myChi*") ! Identify(identifyId) // ワイルドカード
 
       lastSender = sender
     }
 
     case ActorIdentity(`identifyId`, Some(ref)) => {
       log.info("found")
-      lastSender ! ref
+      lastSender ! ref // 検索結果を返す
     }
 
     case ActorIdentity(`identifyId`, None) => {
